3c2a869dd9cd9b59465a7b2a5c381ed65929047b
Meghna Srivastava <meghna.s1906@gmail.com>
2020-05-17 17:10:11 +0200

    new layout v-1.0.1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,99 +1,100 @@
 module.exports = {
-    siteMetadata: {
-        title: 'Suspend Fun: Tech Learnings for Future Self',
-        customHeaderTitle: {
-            title: `Suspend Fun`,
-            extra: `Tech Learnings for Future Self`
-        },
-        author: {
-            name: `Raj Saxena`,
-            summary: `who lives and works in Berlin building useful things.`,
-        },
-        description: `Solving problems in the cloud.`,
-        siteUrl: `https://suspendfun.com`,
-        social: {
-            twitter: `therajsaxena`,
-        },
+  siteMetadata: {
+    title: "Suspend Fun: Tech Learnings for Future Self",
+    customHeaderTitle: {
+      title: `Suspend Fun`,
+      extra: `Tech Learnings for Future Self`,
     },
-    plugins: [{
-            resolve: `gatsby-source-filesystem`,
+    author: {
+      name: `Raj Saxena`,
+      summary: `I live and work in Berlin building useful things.`,
+    },
+    description: `Solving problems in the cloud.`,
+    siteUrl: `https://suspendfun.com`,
+    social: {
+      twitter: `therajsaxena`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-twitter-cards`,
             options: {
-                path: `${__dirname}/content/blog`,
-                name: `blog`,
+              title: "anti/pattern",
+              separator: "|",
+              background: require.resolve("./content/assets/favicon.png"),
+              fontColor: "#228B22",
+              titleFontSize: 96,
+              subtitleFontSize: 60,
+              fontStyle: "monospace",
             },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
+          },
+          {
+            resolve: `gatsby-remark-images`,
             options: {
-                path: `${__dirname}/content/assets`,
-                name: `assets`,
+              maxWidth: 590,
             },
-        },
-        {
-            resolve: `gatsby-transformer-remark`,
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
             options: {
-                plugins: [
-                    {
-                        resolve: `gatsby-remark-twitter-cards`,
-                        options: {
-                          title: 'anti/pattern',
-                          separator: '|', 
-                          background: require.resolve('./content/assets/favicon.png'),
-                          fontColor: '#228B22', 
-                          titleFontSize: 96, 
-                          subtitleFontSize: 60, 
-                          fontStyle: 'monospace', 
-                        },
-                      },
-                    {
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 590,
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-responsive-iframe`,
-                        options: {
-                            wrapperStyle: `margin-bottom: 1.0725rem`,
-                        },
-                    },
-                    `gatsby-remark-prismjs`,
-                    `gatsby-remark-copy-linked-files`,
-                    `gatsby-remark-smartypants`,
-                ],
+              wrapperStyle: `margin-bottom: 1.0725rem`,
             },
-        },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-google-analytics`,
-            options: {
-                trackingId: `UA-150913353-1`,
-            },
-        },
-        `gatsby-plugin-feed`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `Technical Blog by Raj Saxena`,
-                short_name: `Suspend Fun`,
-                start_url: `/`,
-                background_color: `#ffffff`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: `content/assets/favicon.png`,
-            },
-        },
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-plugin-typography`,
-            options: {
-                pathToConfigModule: `src/utils/typography`,
-            },
-        },
-        `gatsby-plugin-typescript`,
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
-    ],
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-150913353-1`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Technical Blog by Raj Saxena`,
+        short_name: `Suspend Fun`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/favicon.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    `gatsby-plugin-typescript`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 }
